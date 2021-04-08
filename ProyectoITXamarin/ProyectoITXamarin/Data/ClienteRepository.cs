6999f2681fa41d89356a10931792afa98f6cfa36
Roberto Obregon <obregonroberto3@gmail.com>
2021-04-08 13:56:34 -0500

    listooooooo

---
+++ ProyectoITXamarin/ProyectoITXamarin/Data/ClienteRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ProyectoITXamarin.DataModel
+{
+    public class ClienteRepository
+    {
+        readonly BindingList<Cliente> clientes;
+
+        public ClienteRepository()
+        {
+            this.clientes = new BindingList<Cliente>();
+        }
+
+        public BindingList<Cliente> Clientes
+        {
+            get { return clientes; }
+        }
+    }
+}
